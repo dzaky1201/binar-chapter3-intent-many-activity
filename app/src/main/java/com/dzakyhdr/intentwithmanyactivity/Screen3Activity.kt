32deca5c817234046ffd3b62ebe925b23b6a4277
dzaky1201 <dzakyhdr@gmail.com>
2022-03-21 20:07:46 +0700

    fix resource string ganjil dan genap

--- app/src/main/java/com/dzakyhdr/intentwithmanyactivity/Screen3Activity.kt
+++ app/src/main/java/com/dzakyhdr/intentwithmanyactivity/Screen3Activity.kt
@@ -10,7 +10,6 @@ import com.dzakyhdr.intentwithmanyactivity.databinding.ActivityScreen3Binding
 class Screen3Activity : AppCompatActivity() {
 
     private lateinit var binding: ActivityScreen3Binding
-    private var status = ""
     private var usia = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,19 +34,17 @@ class Screen3Activity : AppCompatActivity() {
     }
 
     fun viewIdentity(identity: Identity) {
+
         binding.txtYourname.visibility = View.INVISIBLE
         binding.viewDetail4.detail4.visibility = View.VISIBLE
         binding.viewDetail4.txtFullname.text = identity.name
         binding.viewDetail4.txtAlamat.text = identity.alamat
         binding.viewDetail4.txtPekerjaan.text = identity.pekerjaan
-        binding.viewDetail4.txtAge.text = identity.usia.toString()
 
         if (identity.usia % 2 == 0) {
-            status = "Genap"
-            usia = getString(R.string.umur, identity.usia, status)
+            binding.viewDetail4.txtAge.text = getString(R.string.umur, identity.usia, "Genap")
         } else {
-            status = "Ganjil"
-            usia = getString(R.string.umur, identity.usia, status)
+            binding.viewDetail4.txtAge.text = getString(R.string.umur, identity.usia, "Ganjil")
         }
     }
 }
